b889f7151f16e1b58f71485a18eb099190b09ef3
garv190 <garv.jauhari@gmail.com>
2024-03-03 00:17:06 +0530

    form updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,266 @@
-import './App.css';
-import PreNavbar from './components/PreNavbar';
-import Navbar from "./components/Navbar.jsx"
-import {  BrowserRouter as Router,Route,Routes } from "react-router-dom"
-import Slider from "./components/Slider.jsx"
-import data from "./data/data.json"
-import Offers from "./components/Offers.jsx"
-import Heading  from "./components/Heading.jsx"
-import StarProduct from "./components/StarProduct.jsx"
-// import HotAccessoriesMenu  from "./components/HotAccessoriesMenu.jsx"
-import HotAccessories from "./components/HotAccessories.jsx"
-import ProductReviews from "./components/ProductReviews.jsx"
-// import Videos from "./components/Videos.js"
-import Banner from "./components/Banner.jsx"
-import Footer from "./components/Footer.jsx"
-import NavOptios from "./components/NavOptios.jsx"
-import Forms from './components/Forms.jsx';
+// import './App.css';
+// import PreNavbar from './components/PreNavbar';
+// import Navbar from "./components/Navbar.jsx"
+// import {  BrowserRouter as Router,Route,Routes } from "react-router-dom"
+// import Slider from "./components/Slider.jsx"
+// import data from "./data/data.json"
+// import Offers from "./components/Offers.jsx"
+// import Heading  from "./components/Heading.jsx"
+// import StarProduct from "./components/StarProduct.jsx"
+// // import HotAccessoriesMenu  from "./components/HotAccessoriesMenu.jsx"
+// import HotAccessories from "./components/HotAccessories.jsx"
+// import ProductReviews from "./components/ProductReviews.jsx"
+// // import Videos from "./components/Videos.js"
+// import Banner from "./components/Banner.jsx"
+// import Footer from "./components/Footer.jsx"
+// import NavOptios from "./components/NavOptios.jsx"
+// import Forms from './components/Forms.jsx';
 
 
 
 
-function App() {
+// function App() {
 
-  return (
-  <Router>
-       <PreNavbar/>
-       <Routes>
-          {/* <Route path="/" exact component={HomePage} /> */}
-          <Route excat path="/signin"  element={<Forms/>} />
-          </Routes>
-       <Navbar />
+//   return (
+//     <Router>
+//       <PreNavbar />
+//       <Routes>
+//         <Route exact path="/signin" element={<Forms />} />
+//         <Route element={<DefaultLayout />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+
+
+//   return (
+//   // <Router>
+//   <>
+//        <PreNavbar/>
+//        <Routes>
+//           {/* <Route path="/" exact component={HomePage} /> */}
+//           <Route excat path="/signin"  element={<Forms/>} />
+          
+//           </Routes>
+//        <Navbar />
             
-        <NavOptios miPhones={data.miPhones} redmiPhones={data.redmiPhones} tv={data.tv} laptop={data.laptop} fitnessAndLifeStyle={data.fitnessAndLifeStyle} home={data.home} audio={data.audio}  accessories={data.accessories}/>
+//         <NavOptios miPhones={data.miPhones} redmiPhones={data.redmiPhones} tv={data.tv} laptop={data.laptop} fitnessAndLifeStyle={data.fitnessAndLifeStyle} home={data.home} audio={data.audio}  accessories={data.accessories}/>
 
 
-       <Slider start={data.banner.start} />
-       <Offers offer={data.offer} />
-       <Heading text="STAR PRODUCTS"/>
-       <StarProduct starProduct={data.starProduct} />
-       <Heading text="HOT ACCESSORIES" />
-       {/* <HotAccessoriesMenu /> */}
+//        <Slider start={data.banner.start} />
+//        <Offers offer={data.offer} />
+//        <Heading text="STAR PRODUCTS"/>
+//        <StarProduct starProduct={data.starProduct} />
+//        <Heading text="HOT ACCESSORIES" />
+//        {/* <HotAccessoriesMenu /> */}
 
-       <Routes>
+//        <Routes>
 
-<Route exact path="/music"  element = {<HotAccessories music = {data.hotAccessories.music} musicCover = {data.hotAccessoriesCover.music}/>}/>
-</Routes>
-            <Routes>
-       <Route exact path="/smartDevice" element={ <HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice} />} />
-       </Routes>
+// <Route exact path="/music"  element = {<HotAccessories music = {data.hotAccessories.music} musicCover = {data.hotAccessoriesCover.music}/>}/>
+// </Routes>
+//             <Routes>
+//        <Route exact path="/smartDevice" element={ <HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice} />} />
+//        </Routes>
 
-       <Routes>
-       <Route exact path="/home" element={ <HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home} />} />
-       </Routes>
+//        <Routes>
+//        <Route exact path="/home" element={ <HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home} />} />
+//        </Routes>
 
-       <Routes>
-       <Route exact path="/lifestyle" element={ <HotAccessories lifestyle={data.hotAccessories.lifestyle} lifestyleCover={data.hotAccessoriesCover.lifestyle} />} />
-       </Routes>
+//        <Routes>
+//        <Route exact path="/lifestyle" element={ <HotAccessories lifestyle={data.hotAccessories.lifestyle} lifestyleCover={data.hotAccessoriesCover.lifestyle} />} />
+//        </Routes>
 
-       <Routes>
-       <Route exact path="/mobileAccessories" element={ <HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories} />} />
-       </Routes>
+//        <Routes>
+//        <Route exact path="/mobileAccessories" element={ <HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories} />} />
+//        </Routes>
 
 
 
   
 
 
-       <Heading text="PRODUCT REVIEWS"/>
+//        <Heading text="PRODUCT REVIEWS"/>
 
-       <ProductReviews productReviews={data.productReviews}/>
+//        <ProductReviews productReviews={data.productReviews}/>
 
-       {/* <Heading text="VIDEOS" />
+//        {/* <Heading text="VIDEOS" />
       
-      <Videos videos={data.videos} /> */}
+//       <Videos videos={data.videos} /> */}
 
-      <Heading  text="IN THE PRESS"/>
+//       <Heading  text="IN THE PRESS"/>
    
-       <Banner  banner={data.banner}/>
+//        <Banner  banner={data.banner}/>
 
-       <Footer footer={data.footer} />
+//        <Footer footer={data.footer} />
 
 
 
-       
-   </Router>
+//        </> 
+//   //  </Router>
 
+//   );
+
+
+// export default App;
+
+
+
+
+
+
+
+
+
+// import './App.css';
+// import PreNavbar from './components/PreNavbar';
+// import Navbar from "./components/Navbar.jsx"
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+// import Slider from "./components/Slider.jsx"
+// import data from "./data/data.json"
+// import Offers from "./components/Offers.jsx"
+// import Heading from "./components/Heading.jsx"
+// import StarProduct from "./components/StarProduct.jsx"
+// import HotAccessories from "./components/HotAccessories.jsx"
+// import ProductReviews from "./components/ProductReviews.jsx"
+// import Banner from "./components/Banner.jsx"
+// import Footer from "./components/Footer.jsx"
+// import NavOptios from "./components/NavOptios.jsx"
+// import Forms from './components/Forms.jsx';
+
+// function App() {
+
+//   return (
+//     <Router>
+//       <PreNavbar/>
+//       <Routes>
+//         <Route excat path="/signin"  element={<Forms/>} />
+//         <Route path="*" element={<DefaultLayout />} />
+//       </Routes>
+//       )
+//       }
+      
+//       const DefaultLayout = () => {
+// return(
+//       <>
+//         <Navbar />
+//         <NavOptios miPhones={data.miPhones} redmiPhones={data.redmiPhones} tv={data.tv} laptop={data.laptop} fitnessAndLifeStyle={data.fitnessAndLifeStyle} home={data.home} audio={data.audio}  accessories={data.accessories}/>
+//         <Slider start={data.banner.start} />
+//         <Offers offer={data.offer} />
+//         <Heading text="STAR PRODUCTS"/>
+//         <StarProduct starProduct={data.starProduct} />
+//         <Heading text="HOT ACCESSORIES" />
+//         <Routes>
+//           <Route exact path="/music"  element = {<HotAccessories music = {data.hotAccessories.music} musicCover = {data.hotAccessoriesCover.music}/>}/>
+//         </Routes>
+//         <Routes>
+//           <Route exact path="/smartDevice" element={ <HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice} />} />
+//         </Routes>
+//         <Routes>
+//           <Route exact path="/home" element={ <HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home} />} />
+//         </Routes>
+//         <Routes>
+//           <Route exact path="/lifestyle" element={ <HotAccessories lifestyle={data.hotAccessories.lifestyle} lifestyleCover={data.hotAccessoriesCover.lifestyle} />} />
+//         </Routes>
+//         <Routes>
+//           <Route exact path="/mobileAccessories" element={ <HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories} />} />
+//         </Routes>
+//         <Heading text="PRODUCT REVIEWS"/>
+//         <ProductReviews productReviews={data.productReviews}/>
+//         <Heading  text="IN THE PRESS"/>
+//         <Banner  banner={data.banner}/>
+//         <Footer footer={data.footer} />
+//       </>
+//     // </Router>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+import './App.css';
+import PreNavbar from './components/PreNavbar';
+import Navbar from "./components/Navbar.jsx"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Slider from "./components/Slider.jsx"
+import data from "./data/data.json"
+import Offers from "./components/Offers.jsx"
+import Heading from "./components/Heading.jsx"
+import StarProduct from "./components/StarProduct.jsx"
+import HotAccessories from "./components/HotAccessories.jsx"
+import ProductReviews from "./components/ProductReviews.jsx"
+import Banner from "./components/Banner.jsx"
+import Footer from "./components/Footer.jsx"
+import NavOptios from "./components/NavOptios.jsx"
+import Forms from './components/Forms.jsx';
+
+function App() {
+  return (
+    <Router>
+      <PreNavbar/>
+      <Routes>
+        <Route exact path="/signin" element={<Forms />} />
+        <Route path="*" element={<DefaultLayout />} />
+      </Routes>
+
+      <Heading text="HOT ACCESSORIES" />
+      <Routes>
+        <Route exact path="/music"  element = {<HotAccessories music = {data.hotAccessories.music} musicCover = {data.hotAccessoriesCover.music}/>}/>
+      </Routes>
+      <Routes>
+        <Route exact path="/smartDevice" element={ <HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice} />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/home" element={ <HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home} />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/lifestyle" element={ <HotAccessories lifestyle={data.hotAccessories.lifestyle} lifestyleCover={data.hotAccessoriesCover.lifestyle} />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/mobileAccessories" element={ <HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories} />} />
+      </Routes>
+
+
+
+    </Router>
+  );
+}
+
+const DefaultLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <NavOptios miPhones={data.miPhones} redmiPhones={data.redmiPhones} tv={data.tv} laptop={data.laptop} fitnessAndLifeStyle={data.fitnessAndLifeStyle} home={data.home} audio={data.audio}  accessories={data.accessories}/>
+      <Slider start={data.banner.start} />
+      <Offers offer={data.offer} />
+      <Heading text="STAR PRODUCTS"/>
+      <StarProduct starProduct={data.starProduct} />
+      {/* <Heading text="HOT ACCESSORIES" />
+      <Routes>
+        <Route exact path="/music"  element = {<HotAccessories music = {data.hotAccessories.music} musicCover = {data.hotAccessoriesCover.music}/>}/>
+      </Routes>
+      <Routes>
+        <Route exact path="/smartDevice" element={ <HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice} />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/home" element={ <HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home} />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/lifestyle" element={ <HotAccessories lifestyle={data.hotAccessories.lifestyle} lifestyleCover={data.hotAccessoriesCover.lifestyle} />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/mobileAccessories" element={ <HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories} />} />
+      </Routes> */}
+      <Heading text="PRODUCT REVIEWS"/>
+      <ProductReviews productReviews={data.productReviews}/>
+      <Heading  text="IN THE PRESS"/>
+      <Banner  banner={data.banner}/>
+      <Footer footer={data.footer} />
+    </>
   );
 }
 
