5009504d0ff37c8ce9402d569f85af4703f0c814
garv190 <garv.jauhari@gmail.com>
2024-03-03 01:02:36 +0530

    Forms completed

--- src/App.jsx
+++ src/App.jsx
@@ -191,48 +191,37 @@ import data from "./data/data.json"
 import Offers from "./components/Offers.jsx"
 import Heading from "./components/Heading.jsx"
 import StarProduct from "./components/StarProduct.jsx"
-import HotAccessories from "./components/HotAccessories.jsx"
+// import HotAccessories from "./components/HotAccessories.jsx"
 import ProductReviews from "./components/ProductReviews.jsx"
 import Banner from "./components/Banner.jsx"
 import Footer from "./components/Footer.jsx"
 import NavOptios from "./components/NavOptios.jsx"
 import Forms from './components/Forms.jsx';
+import Form1 from './components/Form1.jsx';
+// import Form1 from './components/Form1.jsx';
 
 function App() {
   return (
+  
     <Router>
-      <PreNavbar/>
+     
       <Routes>
         <Route exact path="/signin" element={<Forms />} />
+        {/* <Route exact path="/signup" element={<Form1 />} /> */}
+        <Route exact path="/signup" element={<Form1 />} ></Route>
         <Route path="*" element={<DefaultLayout />} />
       </Routes>
+      </Router>
 
-      <Heading text="HOT ACCESSORIES" />
-      <Routes>
-        <Route exact path="/music"  element = {<HotAccessories music = {data.hotAccessories.music} musicCover = {data.hotAccessoriesCover.music}/>}/>
-      </Routes>
-      <Routes>
-        <Route exact path="/smartDevice" element={ <HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice} />} />
-      </Routes>
-      <Routes>
-        <Route exact path="/home" element={ <HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home} />} />
-      </Routes>
-      <Routes>
-        <Route exact path="/lifestyle" element={ <HotAccessories lifestyle={data.hotAccessories.lifestyle} lifestyleCover={data.hotAccessoriesCover.lifestyle} />} />
-      </Routes>
-      <Routes>
-        <Route exact path="/mobileAccessories" element={ <HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories} />} />
-      </Routes>
-
-
-
-    </Router>
+     
+    
   );
 }
 
 const DefaultLayout = () => {
   return (
     <>
+     <PreNavbar/>
       <Navbar />
       <NavOptios miPhones={data.miPhones} redmiPhones={data.redmiPhones} tv={data.tv} laptop={data.laptop} fitnessAndLifeStyle={data.fitnessAndLifeStyle} home={data.home} audio={data.audio}  accessories={data.accessories}/>
       <Slider start={data.banner.start} />
